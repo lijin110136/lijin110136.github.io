216493116153b526a20b268b684ea570b050a36d
lijin <lijin@huolea.com>
2023-07-29 00:21:28 +0800

    modified

--- openvpn_install.sh
+++ openvpn_install.sh
@@ -161,17 +161,17 @@ firewall_set(){
             firewall-cmd --permanent --zone="${default_zone}" --add-port="${openvpnport}"/tcp
             firewall-cmd --permanent --zone="${default_zone}" --add-port="${openvpnport}"/udp
             firewall-cmd --reload
-
-            iptables -t nat -A POSTROUTING -s 10.8.0.0/24  -j MASQUERADE
-            iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
-            iptables -I FORWARD -j ACCEPT
-            echo "iptable set success"
-            
             service firewalld restart
         else
             echo -e "[${yellow}Warning${plain}] firewalld looks like not running or not installed, please enable port ${openvpnport} manually if necessary."
         fi
     fi
+    sleep 20
+    echo "begin set iptable ip range"
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/24  -j MASQUERADE
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+    iptables -I FORWARD -j ACCEPT
+    echo "finish set iptable ip range"
     echo -e "[${green}Info${plain}] firewall set completed..."
 }
 
